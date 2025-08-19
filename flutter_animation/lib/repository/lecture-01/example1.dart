804caccccce3518e9604f8d13a5d5859693d4b92
Rameez2223 <lalababo68@gmail.com>
2025-08-19 23:28:54 +0500

    Example 1 complete

---
+++ flutter_animation/lib/repository/lecture-01/example1.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class RotatedTriangle extends StatefulWidget {
+  const RotatedTriangle({super.key});
+
+  @override
+  State<RotatedTriangle> createState() => _RotatedTriangleState();
+}
+
+class _RotatedTriangleState extends State<RotatedTriangle>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()..rotateY(_animation.value),
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade600,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                        spreadRadius: 5, blurRadius: 7, offset: Offset(0, 3))
+                  ]),
+            ),
+          );
+        },
+      )),
+    );
+  }
+}
